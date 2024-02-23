f01d48da1a2a713255cfbd8567668cb13b3d32ab
Gyan Prabhat <gyanprabhat07@gmail.com>
2024-02-23 18:27:03 +0530

    initial commit

---
+++ theme/src/config/settings.js
@@ -0,0 +1,8 @@
+export default {
+	title: `Baroda Heritage| Know Your Culture`,
+	description: `Everything about Baroda's culture`,
+	url: `https://barodaheritage.netlify.app`, // No trailing slash!
+	name: `BH`, // The short name of the business or brand name. Used for things like the copyright in the footer.
+  enableThemeSwitcher: false,
+  showLittleSticksPlug: true, // Disable this if you want to remove the Little Sticks plug from the footer. (╯°□°)╯︵ ┻━┻
+};
